705c684a3e30b0042fc206ffcbf0aadbeb0f1949
mochakbar <mochakbar912@gmail.com>
2022-06-29 09:08:46 +0700

    initial commit express typescript

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+import cors from 'cors';
+import * as http from 'http';
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+import {CommonRoutesConfig} from './routes/common.routes.config';
+import {UsersRoutes} from './routes/users.routes.config';
+import debug from 'debug';
+
+const app: express.Application = express();
+
+const server: http.Server = http.createServer(app);
+
+const port = 5000;
+
+const routes: Array<CommonRoutesConfig> = [];
+
+const debugLog: debug.IDebugger = debug('app');
+
+// here we are adding middleware to parse all incoming requests as JSON
+app.use(express.json());
+
+// here we are adding middleware to allow cross-origin requests
+app.use(cors());
+
+// here we are preparing the expressWinston logging middleware configuration,
+// which will automatically log all HTTP requests handled by Express.js
+const loggerOptions: expressWinston.LoggerOptions = {
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.json(),
+    winston.format.prettyPrint(),
+    winston.format.colorize({ all: true })
+  )
+};
+
+if (!process.env.DEBUG) {
+  loggerOptions.meta = false; // when not debugging, log requests as one-liners
+}
+
+// initialize the logger with the above configuration
+app.use(expressWinston.logger(loggerOptions));
+
+// here we are adding the UserRoutes to our array,
+// after sending the Express.js application object to have the routes added to our app!
+routes.push(new UsersRoutes(app));
+
+// this is a simple route to make sure everything is working properly
+const runningMessage = `Server running at http://localhost:${port}`;
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.status(200).send(runningMessage);
+});
+
+server.listen(port, () => {
+  routes.forEach((route: CommonRoutesConfig) => {
+    debugLog(`Routes configured for ${route.getName()}`);
+  });
+  // our only exception to avoiding console.log(), because we
+  // always want to know when the server is done starting up
+  console.log(runningMessage);
+});
