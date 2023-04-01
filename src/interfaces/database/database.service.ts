a05d92ae4f12ec794766b444ce5b1328416424f2
Mochamad Akbar <mochakbar912@gmail.com>
2023-04-01 16:15:54 +0700

    fix(migration): script migration

--- src/interfaces/database/database.service.ts
+++ src/interfaces/database/database.service.ts
@@ -1,22 +1,11 @@
 // import { User } from '@entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { config } from '../../config';
+import { configOrm } from './ormconfig';
 
 @Injectable()
 export class DatabaseService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    return {
-      type: 'postgres',
-      host: config.database.main.host,
-      port: config.database.main.port,
-      username: config.database.main.username,
-      password: config.database.main.password,
-      database: config.database.main.database,
-      logging: process.env.NODE_ENV === 'production' ? false : true,
-      entities: [`${__dirname}/../../domain/*/entities/*.entity{.ts,.js}`],
-      migrations: [`${__dirname}/../../db/migrations/*.ts`],
-      uuidExtension: 'uuid-ossp'
-    };
+    return configOrm as TypeOrmModuleOptions;
   }
 }
